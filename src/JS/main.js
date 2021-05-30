bb2edad3d36fdb4bff1495150ec8efc806bce9e7
Denis Tsibaev <denis.tsibaev@gmail.com>
2021-05-30 14:49:20 +0700

    Update main.js

--- src/JS/main.js
+++ src/JS/main.js
@@ -1,32 +1,47 @@
-  
-"use strict";
+ "use strict";
 
 import gallery from "./gallery-items.js";
 
 const container = document.querySelector(".js-gallery");
 const jsLightbox = document.querySelector(".js-lightbox");
-const origImage = document.querySelector(".lightbox__image");
+const grayBackGround = document.querySelector(".lightbox__content");
+const originalImage = document.querySelector(".lightbox__image");
 const button = document.querySelector(".lightbox__button");
-const greyBackGround = document.querySelector(".lightbox__content");
 
+
+// function createGalery(images) {
+//   const imageGallery = images.reduce(
+//     (item, img) =>
+//       item +
+//       `<li class="gallery__item">
+//     <a class="gallery__link" href="#">
+//       <img class="gallery__image" 
+//       src = '${img.preview}'
+//       data-source = '${img.original}'
+//       alt = '${img.description}'/>
+//     </a>
+//     </li>`,
+//     ""
+//   );
+//   return container.insertAdjacentHTML("afterbegin", imageGallery);
+// }
 
 function createGalery(images) {
-  const imageGallery = images.reduce(
-    (item, img) =>
-      item +
-      `<li class="gallery__item">
-    <a class="gallery__link" href="#">
-      <img class="gallery__image" 
-      src = '${img.preview}'
-      data-source = '${img.original}'
-      alt = '${img.description}'/>
-    </a>
-    </li>`,
-    ""
-  );
-  return container.insertAdjacentHTML("afterbegin", imageGallery);
+	return images.map(({preview, original}) => {
+	 	return
+		`<li class="gallery__item">
+	  <a class="gallery__link" href="#">
+		<img class="gallery__image" 
+		src = '${img.preview}'
+		data-source = '${img.original}'
+		alt = '${img.description}'/>
+	  </a>
+	  </li>`,
+	  ""
+	}));
 }
 
+container.insertAdjacentHTML("afterbegin", imageGallery);
 createGalery(gallery);
 
 container.addEventListener("click", onClick);
@@ -35,9 +50,9 @@ function onClick(event) {
   let checkClick = event.target;
   if (checkClick.classList.contains("gallery__image")) {
     jsLightbox.classList.add("is-open");
-    origImage.setAttribute("src", checkClick.dataset.source);
+    originalImage.setAttribute("src", checkClick.dataset.source);
     button.addEventListener("click", onClickBtn);
-    greyBackGround.addEventListener("click", closeModalClickBackGrount);
+    grayBackGround.addEventListener("click", closeModalClickBackGrount);
     window.addEventListener("keydown", funcPressEsc);
   }
 }
@@ -46,23 +61,24 @@ function onClickBtn(event) {
   let checkBtn = event.target;
   if (!checkBtn.classList.contains("lightbox__image")) {
     jsLightbox.classList.remove("is-open");
-    origImage.removeAttribute("src");
+    originalImage.removeAttribute("src");
     button.removeEventListener("click", onClickBtn);
-    greyBackGround.removeEventListener("click", closeModalClickBackGrount);
+    grayBackGround.removeEventListener("click", closeModalClickBackGround);
     window.removeEventListener("keydown", funcPressEsc);
   }
 }
 
-function closeModalClickBackGrount(event) {
+function closeModalClickBackGround(event) {
   if (event.target === event.currentTarget) {
     jsLightbox.classList.remove("is-open");
-    origImage.removeAttribute("src");
+    originalImage.removeAttribute("src");
   }
 }
 
 function funcPressEsc(event) {
+	// event.keyCode === 27 escape button
   if (event.keyCode === 27) {
     jsLightbox.classList.remove("is-open");
-    origImage.removeAttribute("src");
+    originalImage.removeAttribute("src");
   }
 }
